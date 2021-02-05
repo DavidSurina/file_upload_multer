1a53d3b5a71d3e4d60e71404aa8f7ce63bf4ed3e
DavidSurina <david.surina22@gmail.com>
2021-02-05 12:12:04 +0100

    configured multer and added path/function to handle uploads

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
-const port = 3000;
+let port = 3000;
 
-app.post('/upload-profile-pic', (req, res, next) => {
-    
+var multer = require('multer');
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() + '.img');
+    }
 })
+
+var upload = multer({ storage: storage });
+
+
+app.post('/upload-profile-pic', upload.single('profile_pic'), (req, res, next) => {
+  console.log(req.file)
+});
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
